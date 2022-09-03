29a66de6752c6edc764232fc840475b832c6fec5
Chukwuemeka Ajima <meeky.ae@gmail.com>
2022-09-03 17:15:06 +0200

    fix(colstonjs): fix colstonjs
    - fixes issues with colstonjs framework
    - update the src/colston.ts file
    - update README.md file
    - remove colston from excluded frameworks

--- src/colston.ts
+++ src/colston.ts
@@ -7,13 +7,24 @@ app.get('/', (ctx) => ctx.text('Hi'))
     .post('/json', (ctx) => ctx.json(ctx.request.json()))
     .get('/id/:id', (ctx) => {
         const id = ctx.request.params.id
-        const name = ctx.request.query.name
 
-        ctx.response.headers.set('x-powered-by', xPoweredBy)
+        /**
+         * to handle this, we need to define a separate route:
+         * app.get('/framework?name', (ctx) => ctx.text(ctx.request.query.name))
+         * 
+         * send a request matching this pattern
+         * /framework?name=colston 
+         */
+        const name = ctx.request.query.name // undefined
+
+        ctx.setHeader('x-powered-by', xPoweredBy)
+        // ctx.response.headers.set('x-powered-by', xPoweredBy)
 
         return ctx.text(`${id} ${name}`)
     })
 
-throw new Error('Unable to get it to work for some reason')
+// The issie was fixed in here: https://github.com/ajimae/colstonjs/pull/4 and release in v0.1.0-beta.4
+// It should work fine now
+// throw new Error('Unable to get it to work for some reason')
 
 app.start(3000, () => console.log(`server listening on port :3000`))
