07711a7f52f71d9f783451422d11b36091d8ef14
Revenity <aquaplmc@gmail.com>
2023-05-29 19:26:03 +0700

    Update stricjs.ts

--- src/bun/stricjs.ts
+++ src/bun/stricjs.ts
@@ -1,27 +1,24 @@
-import { Router } from "@stricjs/router";
+import { Router } from '@stricjs/router';
 
+// Prepare headers
 const json = { 
-    headers: {
-        "content-type": "application/json"
-    } 
+    headers: { 'content-type': 'application/json' } 
 };
 const query = {
-    headers: {
-        "x-powered-by": "benchmark"
-    }
+    headers: { 'x-powered-by": "benchmark' }
 }
 
 // Create a router and serve using Bun
 export default new Router()
     // Handle GET request to `/`
-    .get("/", () => new Response("Hi"))
+    .get('/', () => new Response('Hi'))
     // Handle POST request to `/json`
-    .post("/json", async req => new Response(
+    .post('/json', async req => new Response(
         JSON.stringify(await req.json()), json
     ))
     // Return 90 for requests to `/id/90` for instance
-    .get("/id/:id", req => new Response(
-        req.params.id + " " + new URLSearchParams(req.query).get("name"), query
+    .get('/id/:id', req => new Response(
+        req.params.id + ' ' + new URLSearchParams(req.query).get('name'), query
     ))
     // Use the default 404 handler
     .use(404);
