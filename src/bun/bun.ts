c4d4a9a1998e6f0ce7e51dcf46f52359c4b35c19
Revenity <aquaplmc@gmail.com>
2023-07-03 21:18:22 +0700

    Update bun.ts

--- src/bun/bun.ts
+++ src/bun/bun.ts
@@ -1,74 +1,40 @@
-/// <reference types='bun-types' />
-import Bun from 'bun';
-
-const notFound = { status: 404 };
-const json = {
-    headers: {
-        'content-type': 'application/json'
-    }
+const jsonHeaders = { headers: {'Content-Type': 'application/json'} },
+    queryHeaders = { headers: {'x-powered-by': 'benchmark'} },
+    notFound = { status: 404 };
+function toResponse(json: any) {
+    return new Response(JSON.stringify(json), jsonHeaders);
 }
-const query = {
-    headers: {
-        'x-powered-by': 'benchmark'
-    }
-}
-const paramPath = '/id/';
 
-// Serve the server
-export default {
-    port: 3000,
-    async fetch(req) {
-        // The path to check
-        let path: string;
-        // The request query string
-        const method = req.method;
+const dynamicPath = '/id/', dynamicPathLen = dynamicPath.length;
+Bun.serve({
+    fetch(req): Response | Promise<Response> {
+        const url = req.url, 
+            pathIndex = url.indexOf('/', 12),
+            queryIndex = url.indexOf('?', pathIndex + 1),
+            path = queryIndex === -1 
+                ? url.substring(pathIndex)
+                : url.substring(pathIndex, queryIndex),
+            method = req.method;
 
-        // This part parses the path and the query
-        const url = req.url;
-        const pathStart = url.indexOf('/', 12);
-        const pathEnd = url.indexOf('?', pathStart + 1);
-
-        if (pathEnd === -1) {
-            path = url.substring(pathStart);
-        } else 
-            path = url.substring(pathStart, pathEnd);
-
-        // Handle static routes
         switch (path) {
-            // Handle GET request to '/'
             case '/':
-                if (method === 'GET')
+                if (method === 'GET') 
                     return new Response('Hi');
-                return new Response('', notFound);
-            // Handle POST request to '/json'
+                break;
             case '/json':
                 if (method === 'POST')
-                    return new Response(
-                        JSON.stringify(await req.json()), json
-                    );
-                return new Response('', notFound);
-            // Handle GET request to '/id/...'
+                    return req.json().then(toResponse);
+                break;
             default:
-                if (method === 'GET') {
-                    // Check param 
-                    const param = path.indexOf(paramPath);
-                    if (param === -1)
-                        return new Response('', notFound);
-
-                    if (pathEnd === -1) 
-                        return new Response(path.substring(param + 4));
-
+                if (path.startsWith(dynamicPath)) 
                     return new Response(
-                        path.substring(param + 4) + ' ' +
-                        new URLSearchParams(
-                            url.substring(pathEnd + 1)
-                        ).get('name'), query
+                        path.substring(dynamicPathLen) + ' ' + new URLSearchParams(
+                            url.substring(queryIndex + 1)
+                        ).get('name'), queryHeaders
                     );
-                }
-                return new Response('', notFound);
+                break;
         }
-    }
-} as Bun.ServeOptions;
 
-// This shows how hard it is to code a fast server without using a framework
-// I like Stric and Elysia
+        return new Response(null, notFound);
+    }
+});
