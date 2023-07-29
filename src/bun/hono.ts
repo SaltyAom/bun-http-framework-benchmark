59f23ec2aeb2224dd60a5c277f9906a29424c40b
Yusuke Wada <yusuke@cloudflare.com>
2023-07-30 07:22:09 +0900

    fixed Hono scripts for Bun and Node.js

--- src/bun/hono.ts
+++ src/bun/hono.ts
@@ -4,7 +4,7 @@ import { RegExpRouter } from 'hono/router/reg-exp-router'
 const app = new Hono({ router: new RegExpRouter() })
 
 app.get('/', (c) => c.text('Hi'))
-    .post('/json', async (c) => c.json(await c.req.json()))
+    .post('/json', (c) => c.req.json().then(c.json))
     .get('/id/:id', (c) => {
         const id = c.req.param('id')
         const name = c.req.query('name')
