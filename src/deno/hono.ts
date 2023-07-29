e36bfe1b7ed9861d1f18edb9e25aa5823a2f937d
Yusuke Wada <yusuke@cloudflare.com>
2023-07-30 07:12:00 +0900

    add Deno script for Hono

---
+++ src/deno/hono.ts
@@ -0,0 +1,21 @@
+import { Hono, RegExpRouter } from 'https://deno.land/x/hono@v3.2.5/mod.ts'
+
+const app = new Hono({ router: new RegExpRouter() })
+
+app.get('/', (c) => c.text('Hi'))
+	.post('/json', (c) => c.req.json().then(c.json))
+	.get('/id/:id', (c) => {
+		const id = c.req.param('id')
+		const name = c.req.query('name')
+
+		c.header('x-powered-by', 'benchmark')
+
+		return c.text(`${id} ${name}`)
+	})
+
+Deno.serve(
+	{
+		port: 3000
+	},
+	app.fetch
+)
