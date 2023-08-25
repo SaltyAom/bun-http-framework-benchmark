8d89929588a140504d2962f810fdbfbd140a595b
Revenity <aquaplmc@gmail.com>
2023-08-25 16:01:19 +0700

    Update bun.ts

--- src/bun/bun.ts
+++ src/bun/bun.ts
@@ -1,35 +1,30 @@
 const jsonHeaders = { headers: {'Content-Type': 'application/json'} },
     queryHeaders = { headers: {'x-powered-by': 'benchmark'} },
     notFound = { status: 404 };
-function toResponse(json: any) {
-    return new Response(JSON.stringify(json), jsonHeaders);
-}
 
-const dynamicPath = '/id/', dynamicPathLen = dynamicPath.length;
 Bun.serve({
     fetch(req): Response | Promise<Response> {
         const url = req.url, 
-            pathIndex = url.indexOf('/', 12),
-            queryIndex = url.indexOf('?', pathIndex + 1),
+            pathIndex = req.url.indexOf('/', 12) + 1,
+            queryIndex = req.url.indexOf('?', pathIndex),
             path = queryIndex === -1 
-                ? url.substring(pathIndex)
-                : url.substring(pathIndex, queryIndex),
-            method = req.method;
+                ? req.url.substring(pathIndex)
+                : req.url.substring(pathIndex, queryIndex);
 
         switch (path) {
-            case '/':
-                if (method === 'GET') 
+            case '':
+                if (req.method === 'GET') 
                     return new Response('Hi');
                 break;
-            case '/json':
-                if (method === 'POST')
+            case 'json':
+                if (req.method === 'POST')
                     return req.json().then(toResponse);
                 break;
             default:
-                if (path.startsWith(dynamicPath)) 
+                if (path.startsWith('id/')) 
                     return new Response(
-                        path.substring(dynamicPathLen) + ' ' + new URLSearchParams(
-                            url.substring(queryIndex + 1)
+                        path.substring(3) + ' ' + new URLSearchParams(
+                            req.url.substring(queryIndex + 1)
                         ).get('name'), queryHeaders
                     );
                 break;
