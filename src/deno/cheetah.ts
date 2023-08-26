d5fa5ca045e3b111fd3e344441252587beef2647
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-08-27 01:41:12 +0200

    update script for cheetah

--- src/deno/cheetah.ts
+++ src/deno/cheetah.ts
@@ -1,35 +1,14 @@
-import { serve } from "https://deno.land/std@v0.186.0/http/server.ts"
+import cheetah from 'https://deno.land/x/cheetah@v1.5.0/mod.ts'
 
-import cheetah from "https://deno.land/x/cheetah@v0.5.0/mod.ts"
-import zod, { z } from "https://deno.land/x/cheetah@v0.5.0/validator/zod.ts"
+new cheetah()
+  .get('/', () => 'Hi')
+  .get(
+    '/id/:id',
+    (c) => {
+      c.res.header('x-powered-by', 'benchmark')
 
-const app = new cheetah({
-	validator: zod
-})
-	.get("/", () => "Hi")
-	.get(
-		"/id/:id",
-		{
-			query: z.object({
-				name: z.string()
-			})
-		},
-		(ctx) => {
-			ctx.res.header("x-powered-by", "benchmark")
-
-			return `${ctx.req.param("id")} ${ctx.req.query.name}`
-		}
-	)
-	.post(
-		"/json",
-		{
-			body: z.object({
-				hello: z.string()
-			})
-		},
-		(ctx) => ctx.res.json(ctx.req.body)
-	)
-
-serve(app.fetch, {
-	port: 3000
-})
+      return `${c.req.param('id')} ${(c.req.query as { name: string }).name}`
+    }
+  )
+  .post('/json', (c) => c.req.json() as Promise<{ hello: string }>)
+  .serve({ port: 3000 })
