219ec3491a27b948698d191cb8f3167929a53996
Revenity <aquaplmc@gmail.com>
2023-09-09 06:06:15 +0700

    Update bun.ts
    Faster Bun implementation

--- src/bun/bun.ts
+++ src/bun/bun.ts
@@ -1,16 +1,12 @@
-const jsonHeaders = { headers: {'Content-Type': 'application/json'} },
-    queryHeaders = { headers: {'x-powered-by': 'benchmark'} },
-    notFound = { status: 404 };
-
-function toResponse(json: Object) {
-    return new Response(JSON.stringify(json), jsonHeaders)
-}
+const queryHeaders = { headers: {'x-powered-by': 'benchmark'} },
+    notFound = { status: 404 },
+    toRes = Response.json;
 
 Bun.serve({
     fetch(req): Response | Promise<Response> {
         const pathIndex = req.url.indexOf('/', 12) + 1;
         if (pathIndex === req.url.length)
-            if (req.method === 'GET') 
+            if (req.method.charCodeAt(0) === 71) 
                 return new Response('Hi');
         
         const queryIndex = req.url.indexOf('?', pathIndex),
@@ -19,7 +15,7 @@ Bun.serve({
                 : req.url.substring(pathIndex, queryIndex);
 
         if (path === 'json') {
-            if (req.method === 'POST') return req.json().then(toResponse);
+            if (req.method.charCodeAt(2) === 83) return req.json().then(toRes);
         } else if (path.startsWith('id/')) 
             return new Response(
                 path.substring(3) + ' ' + new URLSearchParams(
