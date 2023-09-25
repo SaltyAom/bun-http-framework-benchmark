e8243fb058d0e2b5821a0fcc8f15def577f098d6
Revenity <aquaplmc@gmail.com>
2023-09-25 22:26:35 +0700

    Update hono.ts

--- src/bun/hono.ts
+++ src/bun/hono.ts
@@ -1,17 +1,12 @@
-import { Hono } from 'hono'
-import { RegExpRouter } from 'hono/router/reg-exp-router'
+import { Hono } from 'hono';
 
-const app = new Hono({ router: new RegExpRouter() })
-
-app.get('/', (c) => c.text('Hi'))
-    .post('/json', (c) => c.req.json().then(c.json))
-    .get('/id/:id', (c) => {
-        const id = c.req.param('id')
-        const name = c.req.query('name')
-
-        c.header('x-powered-by', 'benchmark')
-
-        return c.text(`${id} ${name}`)
-    })
-
-export default app
+// Using RegExp is slower in JSC
+// Instead we let the SmartRouter to choose the best router to use
+export default new Hono()
+    .get('/', c => c.text('Hi'))
+    .post('/json', c => c.req.json().then(c.json))
+    .get('/id/:id', c => {
+        c.header('x-powered-by', 'benchmark');
+        
+        return c.text(c.req.param('id') + ' ' + c.req.query('name'));
+    });
