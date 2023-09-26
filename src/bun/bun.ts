db5fc1249de3ba0ad3b13d00a3c67bf475ca279d
Revenity <aquaplmc@gmail.com>
2023-09-26 22:11:26 +0700

    Update bun.ts

--- src/bun/bun.ts
+++ src/bun/bun.ts
@@ -1,12 +1,16 @@
 const queryHeaders = { headers: {'x-powered-by': 'benchmark'} },
     notFound = { status: 404 },
-    toRes = Response.json;
+    jsonHeader = { headers: { 'Content-Type': 'application/json' } };
+
+function jsonRes(d: any) {
+    return new Response(JSON.stringify(d), jsonHeader);
+}
 
 Bun.serve({
     fetch(req): Response | Promise<Response> {
         const pathIndex = req.url.indexOf('/', 12) + 1;
         if (pathIndex === req.url.length)
-            if (req.method.charCodeAt(0) === 71) 
+            if (req.method === 'GET') 
                 return new Response('Hi');
         
         const queryIndex = req.url.indexOf('?', pathIndex),
@@ -15,13 +19,14 @@ Bun.serve({
                 : req.url.substring(pathIndex, queryIndex);
 
         if (path === 'json') {
-            if (req.method.charCodeAt(2) === 83) return req.json().then(toRes);
-        } else if (path.startsWith('id/')) 
-            return new Response(
-                path.substring(3) + ' ' + new URLSearchParams(
-                    req.url.substring(queryIndex + 1)
-                ).get('name'), queryHeaders
-            );
+            if (req.method === 'POST') return req.json().then(jsonRes);
+        } else if (path.startsWith('id/'))
+            if (req.method === 'GET') 
+                return new Response(
+                    path.substring(3) + ' ' + new URLSearchParams(
+                        req.url.substring(queryIndex + 1)
+                    ).get('name'), queryHeaders
+                );
 
         return new Response(null, notFound);
     }
