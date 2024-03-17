52acb03e7ce2225195168ebe87c641c2e28a3f92
Revenity <aquaplmc@gmail.com>
2024-03-17 09:07:11 +0700

    Create byte.ts
    Deno

---
+++ src/deno/byte.ts
@@ -0,0 +1,19 @@
+import { Byte, query, send } from '@bit-js/byte';
+
+// Cache options
+const options = {
+    headers: { 'X-Powered-By': 'benchmark' }
+};
+
+// Extract the 'name' parameter value from query
+const getName = query.value('name');
+
+// Serve directly
+const app = new Byte()
+    .get('/', () => send.body('hi'))
+    // Send ID with query
+    .get('/id/:id', (ctx) => send.body(`${ctx.params.id} ${getName(ctx)}`, options))
+    // Yield body
+    .post('/json', async (ctx) => send.json(await ctx.req.json()));
+
+Deno.serve({ port: 3000 }, app.fetch);
