3046a20bf189c7af5afbf6574567ea0b19f767c5
aquapi <aquaplmc@gmail.com>
2024-06-01 22:01:13 +0700

    Revert "Change my stuff"
    This reverts commit eb23e694437dd9c52c73842cc80e0ba0aa272b1d.

--- src/bun/byte.ts
+++ src/bun/byte.ts
@@ -1,17 +1,15 @@
-import { Byte, query, send } from '@bit-js/byte'
+import { Byte, query, send } from '@bit-js/byte';
 
-const xPoweredBy = ['X-Powered-By', 'benchmark'] as [string, string]
+// Cache options
+const options = { headers: { 'X-Powered-By': 'benchmark' } };
 
 // Extract the 'name' parameter value from query
-const getName = query.get('name')
+const getName = query.get('name');
 
 // Serve directly
 export default new Byte()
-	.get('/', send.body('Hi'))
-	// Send ID with query
-	.get('/id/:id', (ctx) => {
-		ctx.headers.push(xPoweredBy)
-		return ctx.body(`${ctx.params.id} ${getName(ctx)}`)
-	})
-	// Yield body
-	.post('/json', async (ctx) => ctx.json(await ctx.req.json()))
+    .get('/', (ctx) => ctx.text('Hi'))
+    // Send ID with query
+    .get('/id/:id', (ctx) => new Response(`${ctx.params.id} ${getName(ctx)}`, options))
+    // Yield body
+    .post('/json', async (ctx) => ctx.json(await ctx.req.json()));
---
+++ src/bun/stricjs.ts
@@ -0,0 +1,12 @@
+import { Router, macro } from '@stricjs/router';
+import { qs, writeHead } from '@stricjs/utils';
+
+const toRes = Response.json, parse = qs.searchKey('name'), send = writeHead({ 
+    headers: { 'x-powered-by': 'benchmark' } 
+});
+
+export default new Router({ base: 'http://localhost:3000' })
+    .get('/', macro('Hi'))
+    .post('/json', ctx => ctx.json().then(toRes))
+    .get('/id/:id', ctx => send(ctx.params.id + ' ' + parse(ctx)))
+    .use(404)
