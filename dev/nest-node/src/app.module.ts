83b82329e9c9d35f8340fbcea9c9929422309467
bek <ismoiliy98@gmail.com>
2024-11-18 22:09:01 -0600

    feat: add fastify adapter support & make minimal code to run nest on both adapters

---
+++ dev/nest-node/src/app.module.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Get, Module, Param, Post, Query, Res } from '@nestjs/common';
+import type { Response } from 'express';
+import type { FastifyReply } from 'fastify';
+
+@Controller()
+class AppController {
+  @Get('/')
+  getHello(@Res() res: FastifyReply | Response) {
+    res.header('content-type', 'text/plain').send('Hi'); 
+  }
+
+  @Get('/id/:id')
+  getCompose(
+    @Res() res: FastifyReply | Response,
+    @Param('id') id: string,
+    @Query('name') name: string,
+  ) {
+    res.header('content-type', 'text/plain').send(`${id} ${name}`);
+  }
+
+  @Post('/json')
+  postMirror(@Body() body: unknown) {
+    return body;
+  }
+}
+
+@Module({ controllers: [AppController] })
+export class AppModule {}
---
+++ dev/nest-node/src/nest-fastify.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, new FastifyAdapter());
+  await app.listen(3000);
+}
+bootstrap();
