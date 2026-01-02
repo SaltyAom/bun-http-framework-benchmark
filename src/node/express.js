16f468902c5a0aeeabfd47d8fc6954ebf3b25022
Nigro Simone <nigro.simone@gmail.com>
2026-01-02 12:51:41 +0100

    Refactor JSON middleware usage in express routes

--- src/node/express.js
+++ src/node/express.js
@@ -1,11 +1,11 @@
 const express = require('express')
+app.set('x-powered-by', false);
 
 express()
-	.use(express.json())
 	.get('/', (req, res) => {
 		res.setHeader('content-type', 'text/plain').send('Hi')
 	})
-	.post('/json', ({ body }, res) => {
+	.post('/json', express.json(), ({ body }, res) => {
 		res.json(body)
 	})
 	.get('/id/:id', ({ params: { id }, query: { name } }, res) => {
